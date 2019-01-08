3e006cd6735047cd655fca2f2718a214684f2d5d
SweetiRani <sweetsweetirani@gmail.com>
2019-01-08 10:17:22 +0530

    My OnlineProject Setup completed successfully

---
+++ ShoppingBackend/src/main/java/com/sweet/ShoppingBackend/App.java
@@ -0,0 +1,13 @@
+package com.sweet.ShoppingBackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
